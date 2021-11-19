0a88d7a5de03bc616c5d48eebf2086f236953497
Matthew Swann <cv20549@bristol.ac.uk>
2021-11-19 14:33:20 +0000

    Added spring boot server initial setup

---
+++ src/main/java/org/climatearchive/climatearchive/ClimatearchiveApplication.java
@@ -0,0 +1,13 @@
+package org.climatearchive.climatearchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimatearchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimatearchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/climatearchive/climatearchive/ClimatearchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.climatearchive.climatearchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimatearchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
