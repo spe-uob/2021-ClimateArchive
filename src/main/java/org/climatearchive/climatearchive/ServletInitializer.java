c97f4e865cee3fcef35d4c05aa99023f78e76aa0
Matthew Swann <cv20549@bristol.ac.uk>
2021-11-19 14:38:20 +0000

    added basic welcome to root page

--- src/main/java/org/climatearchive/climatearchive/ServletInitializer.java
+++ src/main/java/org/climatearchive/climatearchive/ServletInitializer.java
@@ -2,12 +2,17 @@ package org.climatearchive.climatearchive;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ServletInitializer extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ClimatearchiveApplication.class);
+    @GetMapping("/")
+    @ResponseBody
+    public String test() {
+        return "Welcome to our server";
     }
 
 }
