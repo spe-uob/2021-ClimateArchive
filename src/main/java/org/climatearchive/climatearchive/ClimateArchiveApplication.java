032fd8431f05898e697ce5217dd444bd7fac82cf
Matthew Swann <cv20549@bristol.ac.uk>
2021-11-25 15:43:00 +0000

    now deploys on tomcat

--- src/main/java/org/climatearchive/climatearchive/ClimateArchiveApplication.java
+++ src/main/java/org/climatearchive/climatearchive/ClimateArchiveApplication.java
@@ -2,13 +2,27 @@ package org.climatearchive.climatearchive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class ClimateArchiveApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ClimateArchiveApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ClimateArchiveApplication.class, args);
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String test() {
+        return "Welcome to our server";
     }
 
 }
