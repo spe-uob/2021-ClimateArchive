77313e80270c839d58d88c712a951815a4f97d18
Matthew Swann <cv20549@bristol.ac.uk>
2021-12-06 18:10:17 +0000

    added closest point. we need to fix temp

--- src/main/java/org/climatearchive/climatearchive/Controller.java
+++ src/main/java/org/climatearchive/climatearchive/Controller.java
@@ -5,15 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import ucar.ma2.Array;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.NetcdfFiles;
 import ucar.nc2.Variable;
 import ucar.nc2.dt.grid.GridCoordinate2D;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @RestController
@@ -22,7 +25,7 @@ public class Controller {
     @ResponseBody
     public String test(
 //            @RequestParam("model") String model,
-//            @RequestParam("lat") int lat, @RequestParam("lon") int lon
+            @RequestParam("lat") float lat, @RequestParam("lon") float lon
     ) {
         try (NetcdfFile ncfile = NetcdfFiles.open("./data/tEyea/climate/teyeaa.pdclann.nc")) {
             Variable lats = ncfile.findVariable("latitude");
@@ -32,8 +35,10 @@ public class Controller {
             if (temp == null || rain == null || lats == null || lons == null) {
                 return "Invalid variable name";
             }
-//            Point lookup = findClosestPoint(lat, lon, lats, lons);
-            return temp.read().reduce().toString();
+            Point lookup = findClosestPoint(lat, lon, (float[]) lats.read().copyTo1DJavaArray(), (float[]) lons.read().copyTo1DJavaArray());
+            float[][] tempData = (float[][]) temp.read().reduce().copyToNDJavaArray();
+            float[][] rainData = (float[][]) rain.read().reduce().copyToNDJavaArray();
+            return tempData[lookup.x][lookup.y] + "," + rainData[lookup.x][lookup.y];
         } catch (IOException e) {
             System.out.println(e.getMessage());
             return "Model not found";
@@ -41,8 +46,24 @@ public class Controller {
     }
 
 
-    Point findClosestPoint(int lat, int lon, Variable lats, Variable lons) {
-        return new Point(0, 0);
+    Point findClosestPoint(float lat, float lon, float[] lats, float[] lons) {
+        int x = indexOfClosest(lat, lats);
+        int y = indexOfClosest(lon, lons);
+        return new Point(x, y);
+    }
+
+    int indexOfClosest(float target, float[] arr) {
+        float smallestDiff = Float.MAX_VALUE;
+        int bestIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            float diff = Math.abs(arr[i] - target);
+            if (diff < smallestDiff) {
+                smallestDiff = diff;
+                bestIndex = i;
+            }
+        }
+        return bestIndex;
     }
 
 }
+
