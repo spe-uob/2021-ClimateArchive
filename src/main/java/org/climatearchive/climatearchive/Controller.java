d421e875c6c0685cee2b428844f1d7c3274e5053
Matthew Swann <cv20549@bristol.ac.uk>
2021-12-06 17:20:41 +0000

    added find the closest points function

--- src/main/java/org/climatearchive/climatearchive/Controller.java
+++ src/main/java/org/climatearchive/climatearchive/Controller.java
@@ -8,35 +8,41 @@ import org.springframework.web.bind.annotation.RestController;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.NetcdfFiles;
 import ucar.nc2.Variable;
+import ucar.nc2.dt.grid.GridCoordinate2D;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 
 @RestController
 public class Controller {
     @GetMapping("/getData")
     @ResponseBody
     public String test(
-//            @RequestParam("model") String model, @RequestParam("lat") int lat, @RequestParam("lon") int lon
+//            @RequestParam("model") String model,
+            @RequestParam("lat") int lat, @RequestParam("lon") int lon
     ) {
         try (NetcdfFile ncfile = NetcdfFiles.open("./data/tEyea/climate/teyeaa.pdclann.nc")) {
+            Variable lats = ncfile.findVariable("latitude");
+            Variable lons = ncfile.findVariable("longitude");
             Variable temp = ncfile.findVariable(("temp_mm_1_5m"));
             Variable rain = ncfile.findVariable(("precip_mm_srf"));
-            if (temp == null || rain == null) {
+            if (temp == null || rain == null || lats == null || lons == null) {
                 return "Invalid variable name";
             }
-            System.out.println(temp.read());
+            Point lookup = findClosestPoint(lat, lon, lats, lons);
+            return temp.read().toString();
         } catch (IOException e) {
             System.out.println(e);
             return "Model not found";
         }
-        return "Welcome to our server";
     }
 
-    @GetMapping("/stuff")
-    @ResponseBody
-    public String test2() {
-        return "Welcome to our server 2.0";
+
+    Point findClosestPoint(int lat, int lon, Variable lats, Variable lons) {
+        return new Point(0, 0);
     }
+
 }
