bbdbf2fcd62632f1a9ff861533c3606392d6a926
Jakub Navrátil <73039024+krtekfromkrtis@users.noreply.github.com>
2021-12-08 19:37:40 +0000

    error handling added

--- src/main/java/org/climatearchive/climatearchive/Controller.java
+++ src/main/java/org/climatearchive/climatearchive/Controller.java
@@ -1,15 +1,15 @@
 package org.climatearchive.climatearchive;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.*;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.NetcdfFiles;
 import ucar.nc2.Variable;
-
 import java.awt.*;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Objects;
 
 @RestController
 public class Controller {
@@ -17,29 +17,41 @@ public class Controller {
     @ResponseBody
     public String test(
             @RequestParam("model") String model,
-            @RequestParam("lat") float lat, @RequestParam("lon") float lon
+            @RequestParam("lat") float lat,
+            @RequestParam("lon") float lon
     ) {
         StringBuilder result = new StringBuilder("field,temp,rain");
         for (String field: new String[]{"ann","jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"}) {
             try (NetcdfFile ncfile = NetcdfFiles.open("./data/" + model + "/climate/" + model.toLowerCase() + "a.pdcl" + field + ".nc")) {
-                Variable lats = ncfile.findVariable("latitude");
-                Variable lons = ncfile.findVariable("longitude");
+                float[] lats = (float[]) Objects.requireNonNull(ncfile.findVariable("latitude")).read().copyTo1DJavaArray();
+                float[] lons = (float[]) Objects.requireNonNull(ncfile.findVariable("longitude")).read().copyTo1DJavaArray();
+                if (lat > lats[0] || lat < lats[lats.length-1]) {
+                    return ("Please select a value for latitude between " + lats[lats.length-1] + " & " + lats[0]);
+                }
+                if (lon < lons[0] || lon > lons[lons.length-1]) {
+                    return ("Please select a value for longitude between " + lons[0] +  " & " + lons[lons.length-1]);
+                }
                 Variable temp = ncfile.findVariable(("temp_mm_1_5m"));
                 Variable rain = ncfile.findVariable(("precip_mm_srf"));
-                if (temp == null || rain == null || lats == null || lons == null) {
+                if (temp == null || rain == null ) {
+                    // should it continue and return incomplete data?
+                    //throw new NullPointerException();
                     continue;
                 }
-                Point lookup = findClosestPoint(lat, lon, (float[]) lats.read().copyTo1DJavaArray(), (float[]) lons.read().copyTo1DJavaArray());
+                Point lookup = findClosestPoint(lat, lon, lats, lons);
                 float[][] tempData = (float[][]) temp.read().reduce().copyToNDJavaArray();
                 float[][] rainData = (float[][]) rain.read().reduce().copyToNDJavaArray();
                 result.append("\n").append(tempData[lookup.x][lookup.y]).append(",").append(rainData[lookup.x][lookup.y]);
+            } catch (FileNotFoundException e) {
+                return "Model " + '"' + model + '"' + " not found";
+            } catch (NullPointerException e) {
+                return "Missing data";
             } catch (IOException e) {
-                System.out.println(e.getMessage());
+                return e.getMessage();
             }
         }
         return result.toString();
     }
-
 
     Point findClosestPoint(float lat, float lon, float[] lats, float[] lons) {
         int x = indexOfClosest(lat, lats);
@@ -47,7 +59,7 @@ public class Controller {
         return new Point(x, y);
     }
 
-    int indexOfClosest(float target, float[] arr) {
+    int indexOfClosest(float target, float @NotNull [] arr) {
         float smallestDiff = Float.MAX_VALUE;
         int bestIndex = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -58,6 +70,11 @@ public class Controller {
             }
         }
         return bestIndex;
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public String handleMissingParams(@NotNull MissingServletRequestParameterException e) {
+        return ("Parameter " + '"' + e.getParameterName() + '"' + " not provided");
     }
 
 }
