3817f0b5c0509497f1838d4d2214deb75eac71c7
Matthew Swann <cv20549@bristol.ac.uk>
2021-12-08 18:02:12 +0000

    added model and returns csv format

--- src/main/java/org/climatearchive/climatearchive/Controller.java
+++ src/main/java/org/climatearchive/climatearchive/Controller.java
@@ -24,25 +24,28 @@ public class Controller {
     @GetMapping("/getData")
     @ResponseBody
     public String test(
-//            @RequestParam("model") String model,
+            @RequestParam("model") String model,
             @RequestParam("lat") float lat, @RequestParam("lon") float lon
     ) {
-        try (NetcdfFile ncfile = NetcdfFiles.open("./data/tEyea/climate/teyeaa.pdclann.nc")) {
-            Variable lats = ncfile.findVariable("latitude");
-            Variable lons = ncfile.findVariable("longitude");
-            Variable temp = ncfile.findVariable(("temp_mm_1_5m"));
-            Variable rain = ncfile.findVariable(("precip_mm_srf"));
-            if (temp == null || rain == null || lats == null || lons == null) {
-                return "Invalid variable name";
+        StringBuilder result = new StringBuilder("field,temp,rain");
+        for (String field: new String[]{"ann","jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"}) {
+            try (NetcdfFile ncfile = NetcdfFiles.open("./data/" + model + "/climate/" + model.toLowerCase() + "a.pdcl" + field + ".nc")) {
+                Variable lats = ncfile.findVariable("latitude");
+                Variable lons = ncfile.findVariable("longitude");
+                Variable temp = ncfile.findVariable(("temp_mm_1_5m"));
+                Variable rain = ncfile.findVariable(("precip_mm_srf"));
+                if (temp == null || rain == null || lats == null || lons == null) {
+                    continue;
+                }
+                Point lookup = findClosestPoint(lat, lon, (float[]) lats.read().copyTo1DJavaArray(), (float[]) lons.read().copyTo1DJavaArray());
+                float[][] tempData = (float[][]) temp.read().reduce().copyToNDJavaArray();
+                float[][] rainData = (float[][]) rain.read().reduce().copyToNDJavaArray();
+                result.append("\n").append(tempData[lookup.x][lookup.y]).append(",").append(rainData[lookup.x][lookup.y]);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
             }
-            Point lookup = findClosestPoint(lat, lon, (float[]) lats.read().copyTo1DJavaArray(), (float[]) lons.read().copyTo1DJavaArray());
-            float[][] tempData = (float[][]) temp.read().reduce().copyToNDJavaArray();
-            float[][] rainData = (float[][]) rain.read().reduce().copyToNDJavaArray();
-            return tempData[lookup.x][lookup.y] + "," + rainData[lookup.x][lookup.y];
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            return "Model not found";
         }
+        return result.toString();
     }
 
 
