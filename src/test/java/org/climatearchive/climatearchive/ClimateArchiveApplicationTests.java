9d6ccc07381930a22b5f9bbdf1bf84fa913e254a
Matthew Swann <cv20549@bristol.ac.uk>
2022-01-31 13:28:40 +0000

    added test test

---
+++ src/test/java/org/climatearchive/climatearchive/ClimateArchiveApplicationTests.java
@@ -0,0 +1,21 @@
+package org.climatearchive.climatearchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ClimateArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+    @Test
+    void testBasicDataGetting() {
+        assertEquals(1, 1);
+    }
+
+}
