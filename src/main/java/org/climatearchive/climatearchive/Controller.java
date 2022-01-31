2ef3947ffabce03628fe2cb07093ab13282cc5dd
Matthew Swann <cv20549@bristol.ac.uk>
2022-01-31 13:07:53 +0000

    updated pom file

--- src/main/java/org/climatearchive/climatearchive/Controller.java
+++ src/main/java/org/climatearchive/climatearchive/Controller.java
@@ -32,10 +32,10 @@ public class Controller {
                 try (NetcdfFile ncfile = NetcdfFiles.open("./data/" + model + "/climate/" + model.toLowerCase() + "a.pdcl" + field + ".nc")) {
                     float[] lats = (float[]) Objects.requireNonNull(ncfile.findVariable("latitude")).read().copyTo1DJavaArray();
                     float[] lons = (float[]) Objects.requireNonNull(ncfile.findVariable("longitude")).read().copyTo1DJavaArray();
-                    if (lat > lats[0] || lat < lats[lats.length - 1]) {
+                    if (lat > lats[0] || lat < lats[lats.length - 1]) { //todo change lat to mod so that all values are accepted
                         return ("Please select a value for latitude between " + lats[lats.length - 1] + " & " + lats[0]);
                     }
-                    if (lon < lons[0] || lon > lons[lons.length - 1]) {
+                    if (lon < lons[0] || lon > lons[lons.length - 1]) { //todo change lon to mod so that all values are accepted
                         return ("Please select a value for longitude between " + lons[0] + " & " + lons[lons.length - 1]);
                     }
                     Variable temp = ncfile.findVariable(("temp_mm_1_5m"));
