86554e88d35648533aa0f7020666d7c956ef3742
Matthew Swann <cv20549@bristol.ac.uk>
2022-01-31 13:54:13 +0000

    added failing test

--- src/test/java/org/climatearchive/climatearchive/ClimateArchiveApplicationTests.java
+++ src/test/java/org/climatearchive/climatearchive/ClimateArchiveApplicationTests.java
@@ -1,9 +1,7 @@
 package org.climatearchive.climatearchive;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
