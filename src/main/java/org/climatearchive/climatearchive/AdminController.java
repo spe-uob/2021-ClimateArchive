f9747eaadad7a564e061a80d316f3ef91a827c57
Matthew Swann <cv20549@bristol.ac.uk>
2022-02-04 15:58:06 +0000

    started implementing the admin controller

--- src/main/java/org/climatearchive/climatearchive/AdminController.java
+++ src/main/java/org/climatearchive/climatearchive/AdminController.java
@@ -3,14 +3,28 @@ package org.climatearchive.climatearchive;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AdminController {
 
     @PostMapping
-    public ResponseEntity<Object> addModels() { // add models to sqlite db
-        return new ResponseEntity<>("OK", HttpStatus.OK);
+    public ResponseEntity<Object> addModels(@RequestBody List<String> models) { // add models to sqlite db
+        List<String> failedModels = new ArrayList<>();
+        for (String m: models) {
+            // add model m to sql database
+        }
+        if (failedModels.isEmpty()) {
+            return new ResponseEntity<>("OK", HttpStatus.OK);
+        } else if (failedModels.size() != models.size()) {
+            return new ResponseEntity<>("Failed to add:\n" + String.join("\n", failedModels), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Couldn't add any models to DB", HttpStatus.EXPECTATION_FAILED);
+        }
     }
 
 }
