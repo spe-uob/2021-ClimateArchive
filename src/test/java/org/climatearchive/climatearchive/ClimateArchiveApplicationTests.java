808ec2f9905332d865e5cf39bc1666484942af28
Matthew Swann <cv20549@bristol.ac.uk>
2022-04-01 15:57:35 +0100

    started implementing new tests. Old model.data is removed but needs replacing before all the tests will pass

--- src/test/java/org/climatearchive/climatearchive/ClimateArchiveApplicationTests.java
+++ src/test/java/org/climatearchive/climatearchive/ClimateArchiveApplicationTests.java
@@ -1,15 +1,18 @@
 package org.climatearchive.climatearchive;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.File;
+import java.io.IOException;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,6 +23,13 @@ class ClimateArchiveApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @BeforeAll
+    static void removeOldModels() {
+        if (new File("model.data").delete()) {
+            System.out.println("Removed old file");
+        }
+    }
 
     @Test
     void contextLoads() {}
