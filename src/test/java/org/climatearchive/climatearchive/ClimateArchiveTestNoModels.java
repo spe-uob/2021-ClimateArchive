e9bbce2c6e5ebd3ecc3449cb2af812fcf4847f7c
Matthew Swann <cv20549@bristol.ac.uk>
2022-04-11 15:07:48 +0100

    Split new tests into two. Those with models and those without. Updated git ignore as tests should work independently of model.data as long as the test datasets are provided

--- src/test/java/org/climatearchive/climatearchive/ClimateArchiveTestNoModels.java
+++ src/test/java/org/climatearchive/climatearchive/ClimateArchiveTestNoModels.java
@@ -1,6 +1,7 @@
 package org.climatearchive.climatearchive;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.File;
-import java.io.IOException;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ClimateArchiveApplicationTests {
+public class ClimateArchiveTestNoModels {
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,17 +31,20 @@ class ClimateArchiveApplicationTests {
         }
     }
 
+    @Order(0)
     @Test
     void contextLoads() {}
 
+    @Order(1)
     @Test
     void testBasicDataGetting() throws Exception {
         mockMvc.perform(get("/getData?model=tEyea&lat=50&lon=50"))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("field,temp,rain\nann,283.55814,1.0425966E-5\njan,267.05478,1.7415154E-5\nfeb,269.44598,1.44473015E-5\nmar,274.88287,7.2331854E-6\napr,283.03055,8.1359985E-6\nmay,292.10703,7.907912E-6\njun,299.283,8.6910995E-6\njul,301.8999,9.718774E-6\naug,299.81317,3.5989037E-6\nsep,292.23615,3.6016913E-6\noct,281.3395,1.07414835E-5\nnov,272.86063,1.5174153E-5\ndec,268.7442,1.8445933E-5")));
+                .andExpect(status().is(400))
+                .andExpect(content().string(containsString("Model " + '"' + "tEyea" + '"' + " not found")));
     }
 
+    @Order(2)
     @Test
     void testParameterNotProvided() throws Exception {
         mockMvc.perform(get("/getData"))
@@ -58,6 +61,7 @@ class ClimateArchiveApplicationTests {
                 .andExpect(content().string(containsString("Parameter " + '"' + "lon" + '"' + " not provided.")));
     }
 
+    @Order(3)
     @Test
     void testModelNotFound() throws Exception {
         mockMvc.perform(get("/getData?model=modelNameThatDoesntExist&lat=50&lon=50"))
@@ -65,4 +69,6 @@ class ClimateArchiveApplicationTests {
                 .andExpect(status().is(400))
                 .andExpect(content().string(containsString("Model " + '"'  + "modelNameThatDoesntExist" + '"' + " not found.")));
     }
+
+
 }
